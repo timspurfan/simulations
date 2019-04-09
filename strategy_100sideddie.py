a1c68a4cfdc8560c1d3bbfa72c38245f5aaac746
timspurfan <timspurfan@yahoo.com>
2019-04-08 23:35:15 -0400

    Added solution to strategy_100sideddie.py

--- strategy_100sideddie.py
+++ strategy_100sideddie.py
@@ -9,8 +9,17 @@ What is the expected value of the game?
 
 Answer:
 87
-(continuous solution gives 86.6ish...suppose game has EV = E...then will roll again if 
-roll is less than E-1)
+(continuous solution gives 86.6ish)
+
+Solution:
+Suppose game has EV = E.  Then will roll again if roll is less than E-1.  For simplicty, rather than drawing from the discrete distribution, let's suppose that our die gives a uniform distribution between [0,100].  Thus, the probability our roll is less than E-1 is
+(E-1)/100.  So, we have the following relation: 
+E = (E-1)/(100) * (E-1) + (100-E+1)/(100) * (100+E-1/2).
+The first case is the probability of rolling a number below E-1, which would cause us to
+re-roll, and then resulting in an EV of E-1 (Since you're playing the same game but now down $1).
+The second case 
+re-rolling * ev of re-rolling and the second is the prob of not re
+Solving for E in (0,100) gives 86.86ish.
 '''
 
 '''
